932ef85b46af835c2f8a3dded33b1da7e30ced18
Kalen Jones <92343379+kalen3@users.noreply.github.com>
2021-12-06 14:26:56 +0000

    Test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int MAX_LEN = 6;
+const int MAX_LEN = 50;
 const int OFFSET = 'a' - 'A';
 
 //function prototypes
@@ -21,6 +21,10 @@ int main()
   int string_len  = 0;
   int numOfValid = 0;
   int numOfInvalid = 0;
+  int plusCount = 0;
+  int minusCount = 0;
+  int decimalCount = 0;
+  int eCount = 0;
 
   ifstream myfile ("example.txt");
 
@@ -40,7 +44,7 @@ int main()
       {
         if (Isdigit(check1[i]))
         {
-          cout << "Valid \n";
+          //cout << "Valid \n";
           string_len++;
           numOfValid++;
         }
@@ -49,51 +53,71 @@ int main()
         {
           if (check1[i] >= 'e' || check1[i] <= 'E') //condtion for input before
           {
+            eCount++;//Create conditions
             //After checking for e or E create conditions for after e or E
             if (Isdigit(check1[i + 1]))
             {
-              cout << "Valid \n";
+              //cout << "Valid \n";
               numOfValid++;
-              //string_len++;
             }
             else if ((check1[i+ 1] == '+') || (check1[i + 1] == '-'))
             {
+              //Plus or minus counter
+              if (check1[i+ 1] == '+')
+              {
+                plusCount++;//Create conditions
+              }
+              else if (check1[i + 1] == '-')
+              {
+                minusCount++;//Create conditions
+              }
+              
+              //Valid counter
               if (Isdigit(check1[i + 2]))
               {
-                cout << "Valid. \n";
+                //cout << "Valid. \n";
                 numOfValid++;
-                //string_len++;
               }
               else 
               {
-                cout << "Invalid. \n";
+                //cout << "Invalid. \n";
                 numOfInvalid++;
-                //string_len++;
               }
             }
             else
             {
-              cout << "invalid. \n";
+              //cout << "invalid. \n";
               numOfInvalid++;
-              //string_len++;
             }
           }
           //Else statement for case that it passes alphabet but fails
           else 
           {
-            cout << "Invalid?(e) \n";
+            //cout << "Invalid?(e) \n";
             numOfInvalid++;
           }
           string_len++;
         }
 
         //else if for + - starting
-        else if ((check1[i] == '+') || (check1[i] == '-'))
+        else if ((check1[0] == '+') || (check1[0] == '-'))
         {
+          //Plus or minus counter
+          if (check1[0] == '+')
+          {
+            plusCount++;//Create conditions
+            //cout << "Plus: " << plusCount << endl;
+          }
+          else if (check1[0] == '-')
+          {
+            minusCount++;//Create conditions
+            //cout << "Minus: " << minusCount << endl;
+          }
+
           //Statement for what's after i
           if (Isdigit(check1[i + 1]))//maybe begin offset
           {
-            cout << "Valid. \n";
+            //cout << "Valid. \n";
             numOfValid++;
           }
 
@@ -101,9 +125,8 @@ int main()
 
           else 
           {
-            cout << "Invalid. \n";
+            //cout << "Invalid. \n";
             numOfInvalid++;
-            //string_len++;
           }
           string_len++;
         }
@@ -111,23 +134,21 @@ int main()
         //else if for . (only one .)
         else if (check1[i] == '.')
         {
+          decimalCount++;//Create conditions
           if (Isdigit(check1[i - 1]))
           {
-            cout << "valid \n";
+            //cout << "valid \n";
             numOfValid++;
-            //string_len++;
           }
           else if (Isdigit(check1[i + 1]))
           {
-            cout << "valid \n";
+            //cout << "valid \n";
             numOfValid++;
-            //string_len++;
           }
           else
           {
-            cout << "Invalid \n";
+            //cout << "Invalid \n";
             numOfInvalid++;
-            //string_len++;
           }
           string_len++;
         }
@@ -135,11 +156,9 @@ int main()
         //else statement saying anything else invalid
         else
         {
-          cout << "nvalid. \n";
+          //cout << "nvalid. \n";
           numOfInvalid++;
           //string_len++;
-          //invalid++;
-          //maybe use this else as strings MAX_LEN
           i = MAX_LEN;
         }
       }
@@ -147,6 +166,15 @@ int main()
       //Change b/c if valid = 0
       if (numOfValid >= numOfInvalid)
       {
+        //New conditions for + - . e
+        if ((plusCount > 1) || (minusCount > 1) || (decimalCount > 1) || (eCount > 1))
+        {
+          cout << "This is Not a literal. \n";//Create more conditions for + -
+        }
+        else
+        {
+          cout << "This is a literal. \n";
+        }
         cout << "This is a literal. \n";
       }
       else
@@ -156,12 +184,18 @@ int main()
       //cout << "Valid: " << numOfValid << endl;
       //cout << "Invalid: " << numOfInvalid << endl;
       //cout << "Len: " << string_len << endl;
+      cout << "Plus: " << plusCount << endl;
+      cout << "Minus: " << minusCount << endl;
       cout << endl;
 
       //reset counters
       string_len = 0;
       numOfValid = 0;
       numOfInvalid = 0;
+      plusCount = 0;
+      minusCount = 0;
+      decimalCount = 0;
+      eCount = 0;
     }
     myfile.close();
   }
